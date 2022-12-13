40478b780476877497ca088e9d995253638b2519
Dima-sudo <dima.borodinw@gmail.com>
2022-12-13 21:39:12 +0200

    fix: testing deployment

--- webpack.config.js
+++ webpack.config.js
@@ -169,51 +169,51 @@ module.exports = (_env, argv) => {
             new Dotenv(),
         ].filter(Boolean),
 
-        optimization: {
-            minimize: isProduction,
-            minimizer: [
-                new TerserWebpackPlugin({
-                    terserOptions: {
-                        compress: {
-                            comparisons: false,
-                        },
-                        mangle: {
-                            safari10: true,
-                        },
-                        output: {
-                            comments: false,
-                            ascii_only: true,
-                        },
-                        warnings: false,
-                    },
-                }),
-                new OptimizeCssAssetsPlugin(),
-            ],
-            splitChunks: {
-                chunks: 'all',
-                minSize: 0,
-                maxInitialRequests: 20,
-                maxAsyncRequests: 20,
-                cacheGroups: {
-                    vendors: {
-                        test: /[\\/]node_modules[\\/]/,
-                        name(module, chunks, cacheGroupKey) {
-                            const packageName = module.context.match(
-                                /[\\/]node_modules[\\/](.*?)([\\/]|$)/
-                            )[1];
-                            return `${cacheGroupKey}.${packageName.replace(
-                                '@',
-                                ''
-                            )}`;
-                        },
-                    },
-                    common: {
-                        minChunks: 2,
-                        priority: -10,
-                    },
-                },
-            },
-            runtimeChunk: 'single',
-        },
+        // optimization: {
+        //     minimize: isProduction,
+        //     minimizer: [
+        //         new TerserWebpackPlugin({
+        //             terserOptions: {
+        //                 compress: {
+        //                     comparisons: false,
+        //                 },
+        //                 mangle: {
+        //                     safari10: true,
+        //                 },
+        //                 output: {
+        //                     comments: false,
+        //                     ascii_only: true,
+        //                 },
+        //                 warnings: false,
+        //             },
+        //         }),
+        //         new OptimizeCssAssetsPlugin(),
+        //     ],
+        //     splitChunks: {
+        //         chunks: 'all',
+        //         minSize: 0,
+        //         maxInitialRequests: 20,
+        //         maxAsyncRequests: 20,
+        //         cacheGroups: {
+        //             vendors: {
+        //                 test: /[\\/]node_modules[\\/]/,
+        //                 name(module, chunks, cacheGroupKey) {
+        //                     const packageName = module.context.match(
+        //                         /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+        //                     )[1];
+        //                     return `${cacheGroupKey}.${packageName.replace(
+        //                         '@',
+        //                         ''
+        //                     )}`;
+        //                 },
+        //             },
+        //             common: {
+        //                 minChunks: 2,
+        //                 priority: -10,
+        //             },
+        //         },
+        //     },
+        //     runtimeChunk: 'single',
+        // },
     };
 };
